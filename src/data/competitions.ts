c247b021b9359b321f19bc9e8dbbb5fcec52ef1d
irfansyhptra <irfan19ksp@gmail.com>
2026-09-10 20:32:16 +0700

    update competition guidebook links

--- src/data/competitions.ts
+++ src/data/competitions.ts
@@ -5,7 +5,7 @@ export const competitionData = [
     description:
       "Design exceptional user experiences and interfaces for digital products. Showcase your creativity and design thinking skills!",
     keywords: ["Team-based", "Design Thinking", "Prototyping"],
-    guidebookLink: "https://drive.google.com/file/d/14Xo5OIGHbSMvgfu2GHnVTJpEs4r9ZfSM/view?usp=drivesdk",
+    guidebookLink: "https://drive.google.com/file/d/1aqQVl4uhJeZbRYI-qAf8ihhbU0Vix3He/view?usp=sharing",
     prizepool: "IDR 2.500.000",
     teamMin: 1,
     teamMax: 3,
@@ -17,7 +17,7 @@ export const competitionData = [
     description:
       "20+ hour intensive coding marathon where innovation meets implementation. Build solutions that matter.",
     keywords: ["Team-based", "Innovation"],
-    guidebookLink: "https://drive.google.com/file/d/1xOk1Wf1PsVOuC0aldLm-gXiN62IAxGrV/view?usp=drivesdk",
+    guidebookLink: "https://drive.google.com/file/d/1ohWWhlBDe-32mqLTGEOql1B1Q7A_kSQs/view?usp=sharing",
     prizepool: "IDR 15.000.000",
     teamMin: 2,
     teamMax: 3,
@@ -29,7 +29,7 @@ export const competitionData = [
     description:
       "Analyze complex data sets, build predictive models, and extract valuable insights to solve real-world problems.",
     keywords: ["Team-based", "Data Analysis", "Machine Learning", "Statistics"],
-    guidebookLink: "https://drive.google.com/file/d/1NRFOCq03TBMOJ0WHXR84rEAbmqbzS3Pl/view?usp=drive_link",
+    guidebookLink: "https://drive.google.com/file/d/1R_jdv8JXp3huWJCFCiECN7m8f-_rK29H/view?usp=sharing",
     prizepool: "IDR 4.000.000",
     teamMin: 2,
     teamMax: 3,
